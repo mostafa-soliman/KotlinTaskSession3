cb0ba1fe9e6db03317598c00b2462dc02e5c1225
Eihab-Ahmed <eihab.ahmed@boniantech.com>
2024-07-28 15:01:57 +0300

    1- Ask the user to enter one integer (Int)

---
+++ src/Main.kt
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+//1- Ask the user to enter one integer (Int)
+    val oneInt :Int?
+    println("enter one integer")
+    oneInt = readln().substring(0,1).toIntOrNull()
+    println("enter one integer : ${oneInt}")
+
+}
