6b489535fdd2384dbc0b152a87c4753f33f7a0a5
Eihab-Ahmed <eihab.ahmed@boniantech.com>
2024-07-28 17:37:46 +0300

    3: Calculate the quotient of the double divided by the integer.

--- src/Main.kt
+++ src/Main.kt
@@ -12,4 +12,15 @@ fun main() {
     println("enter one Double")
     oneDouble = readln().substring(0,1).toDoubleOrNull()
     println("enter one Double : ${oneDouble}")
+
+//3: Calculate the quotient of the double divided by the integer.
+println("Calculate the quotient of the double divided by the integer.")
+    if(oneInt!== 0 && oneInt !=null && oneDouble != null )  {
+        val calDiv: Double = oneDouble/oneInt
+        println("The quotient is: $calDiv")
+    } else {
+        println("Cannot perform division. Please ensure the integer is not null or zero, and the double is not null.")
+    }
+
 }
+
